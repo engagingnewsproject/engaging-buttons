a669b276275558b4aa9fb11aeb5e02a211b00496
Jerry <jones.jeremydavid@gmail.com>
2015-09-30 13:40:32 -0500

    added enp_button_slugs and enp_button_ to wp_options on save

--- admin/settings/respect-button-settings.php
+++ admin/settings/respect-button-settings.php
@@ -51,6 +51,59 @@ function set_enp_button_content_type_values($value) {
 }
 
 
+// We can't register dynamically named variables, so we're going to
+// create everything under enp_buttons and parse from there
+add_filter( 'pre_update_option_enp_buttons', 'set_enp_buttons_values', 10, 2 );
+function set_enp_buttons_values($values) {
+
+
+    // Save slugs enp_button_slugs = array('respect', 'important', 'recommend');
+        $enp_button_slugs = array();
+        foreach($values as $value) {
+            $enp_button_slugs[] = $value['btn_slug'];
+        }
+        // TODO: Check to see if a slug has a enp_button_$slug entry.
+        //       We may not want to overwrite this entirely otherwise
+        update_option('enp_button_slugs', $enp_button_slugs);
+
+    // TODO: Set Old Buttons Slugs to active = 0 if it's no longer in use
+
+    // Save/Create enp_button_$slug
+
+        // Set all btn_type options
+        $enp_btn = new Enp_Button();
+        $registered_content_types = $enp_btn->registeredContentTypes();
+        $i = 0;
+        foreach($values as $value) {
+
+            // Set all btn_type options so they're false if they weren't checked
+            foreach($registered_content_types as $type) {
+                // set it to false if it wasn't set
+                if(empty($value['btn_type'][$type['slug']])) {
+                    $value['btn_type'][$type['slug']] = false;
+                    // update the oringal $values too
+                    $values[$i]['btn_type'][$type['slug']] = false;
+                }
+            }
+            // dynamically named enp_button_$slug so we can access just one
+            // field on the front end or for objects, as needed
+            ${'enp_button_'.$value['btn_slug']} = array(
+                                                        'slug' => $value['btn_slug'],
+                                                        'name' => ucfirst($value['btn_slug']),
+                                                        'btn_type' => $value['btn_type'],
+                                                        'locked' => false, // TODO: Set to true if count is more than 0 (has clicks)
+                                                  );
+
+
+            // update the database entry
+            update_option('enp_button_'.$value['btn_slug'], ${'enp_button_'.$value['btn_slug']});
+
+            $i++;
+        }
+
+    // Save the entire enp_buttons as is so we have everything in one place if we need it
+    return $values;
+}
 
 
 // Create settings fields.
@@ -58,14 +111,66 @@ add_action( 'admin_init', 'enp_respect_button_data' );
 function enp_respect_button_data() {
 
     // button type
-    register_setting( 'enp_respect_button_settings', 'enp_button_type' );
-    // content type
-    register_setting( 'enp_respect_button_settings', 'enp_button_content_type' );
+    register_setting( 'enp_respect_button_settings', 'enp_buttons' );
+
     // other settings
     register_setting( 'enp_respect_button_settings', 'enp_button_must_be_logged_in' );
     register_setting( 'enp_respect_button_settings', 'enp_button_allow_data_tracking' );
     register_setting( 'enp_respect_button_settings', 'enp_button_promote_enp' );
 }
+
+function buttonCreateHTML($i, $enp_buttons, $enp_btn) {?>
+        <table class="form-table">
+            <tbody>
+                <tr>
+                        <th scope="row">
+                            <label for="enp-button-type">Button</label>
+                        </th>
+                        <td>
+                            <fieldset>
+                                <label>
+                                    <input type="radio" name="enp_buttons[<?php echo $i;?>][btn_slug]" aria-describedby="enp-button-slug-description" value="respect" <?php checked('respect', $enp_buttons[$i]["btn_slug"]);?> /> Respect
+                                </label>
+                                <br/>
+                                <label>
+                                    <input type="radio" name="enp_buttons[<?php echo $i;?>][btn_slug]" aria-describedby="enp-button-slug-description" value="recommend" <?php checked('recommend', $enp_buttons[$i]["btn_slug"]);?> /> Recommend
+                                </label>
+                                <br/>
+                                <label>
+                                    <input type="radio" name="enp_buttons[<?php echo $i;?>][btn_slug]" aria-describedby="enp-button-slug-description" value="important" <?php checked('important', $enp_buttons[$i]["btn_slug"]);?>/> Important
+                                </label>
+
+                                <p id="enp-button-slug-description"class="description">Which button do you want to use on your site?</p>
+
+                                <p class="description">Have an idea for other button text options? Let us know! ____@engagingnewsproject.org
+                            </fieldset>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">
+                            <label for="enp-button-content-type">How to Use this Button</label>
+                        </th>
+                        <td>
+                            <fieldset>
+                                <?  $registered_content_types = $enp_btn->registeredContentTypes();
+                                    $checklist_html = '';
+
+                                    foreach($registered_content_types as $content_type) {
+                                        $checklist_html .= '<label>
+                                                                <input type="checkbox" name="enp_buttons['.$i.'][btn_type]['.$content_type['slug'].']" value="1" '.checked(true, $enp_buttons[$i]['btn_type'][$content_type['slug']], false).' aria-describedby="enp-button-content-type-description"/> '.$content_type['label_name'].'
+                                                            </label>
+                                                            <br/>';
+                                    }
+
+                                    echo $checklist_html;?>
+                            </fieldset>
+                            <p id="enp-button-content-type-description" class="description">Which kinds of content do you want the button(s) to display for?</p>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <hr>
+<? }
 
 /**
 * Step 3: Create the markup for the options page
@@ -97,45 +202,28 @@ function enp_respect_button_page() { ?>
         $btn_allow_data_tracking = get_option('enp_button_allow_data_tracking');
         $btn_promote_enp = get_option('enp_button_promote_enp');
 
+
+
+        $enp_buttons = get_option('enp_buttons');
+        echo '<br/><br/>';
+        var_dump($enp_buttons);
+
         ?>
 
+        <?
+        $i = 0;
+        foreach($enp_buttons as $enp_button) {
+            buttonCreateHTML($i, $enp_buttons, $enp_btn);
+            $i++;
+        }
+        // if $i still equals 0, then we don't have ANY buttons, and we still need to
+        // create one
+        if($i === 0) {
+            buttonCreateHTML($i, $enp_buttons, $enp_btn);
+        }
+        ?>
         <table class="form-table">
             <tbody>
-                <tr>
-                    <th scope="row">
-                        <label for="enp-button-type">Buttons</label>
-                    </th>
-                    <td>
-                        <fieldset>
-                            <label>
-                                <input type="checkbox" name="enp_button_type[respect]" aria-describedby="enp-button-type-description" value="true" <?php checked(true, $btn_types["respect"]);?> /> Respect
-                            </label>
-                            <br/>
-                            <label>
-                                <input type="checkbox" name="enp_button_type[recommend]" aria-describedby="enp-button-type-description" value="true" <?php checked(true, $btn_types["recommend"]);?> /> Recommend
-                            </label>
-                            <br/>
-                            <label>
-                                <input type="checkbox" name="enp_button_type[important]" aria-describedby="enp-button-type-description" value="true" <?php checked(true, $btn_types["important"]);?>/> Important
-                            </label>
-
-                            <p id="enp-button-type-description"class="description">Which buttons do you want to use on your site?</p>
-
-                            <p class="description">Have an idea for other button text options? Let us know! ____@engagingnewsproject.org
-                        </fieldset>
-                    </td>
-                </tr>
-                <tr>
-                    <th scope="row">
-                        <label for="enp-button-content-type">How to Use the Buttons</label>
-                    </th>
-                    <td>
-                        <fieldset>
-                            <? echo $enp_respect->buildContentTypeChecklist();?>
-                        </fieldset>
-                        <p id="enp-button-content-type-description" class="description">Which kinds of content do you want the button(s) to display for?</p>
-                    </td>
-                </tr>
                 <tr>
                     <th scope="row">
                         Respect Button Plugin Setting
