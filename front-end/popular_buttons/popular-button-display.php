b6bb048d705f474c08c6bb22601477d6206633c2
Jerry <jones.jeremydavid@gmail.com>
2015-11-05 14:05:21 -0600

    added minor popular button display css style and fixed remove filter function

--- front-end/popular_buttons/popular-button-display.php
+++ front-end/popular_buttons/popular-button-display.php
@@ -9,8 +9,10 @@
 
 // remove all the filters on this file
 function enp_remove_popular_posts_filters() {
-    remove_filter('enp_default_pop_posts_section_wrap');
-    remove_filter('enp_default_pop_posts_section_title');
+    remove_filter('enp_popular_posts_loop_wrap', 'enp_default_pop_posts_loop_wrap');
+    remove_filter('enp_popular_posts_loop_before_html', 'enp_default_pop_posts_loop_before');
+    remove_filter('enp_popular_posts_loop_after_html', 'enp_default_pop_posts_loop_after');
+    remove_filter('enp_popular_post_html', 'enp_default_pop_post_html');
 }
 
 // Wrap HTML for each popular post section
@@ -24,20 +26,28 @@ function enp_default_pop_posts_loop_wrap($html, $pop_posts) {
 add_filter('enp_popular_posts_loop_wrap', 'enp_default_pop_posts_loop_wrap', 10, 2 );
 
 
-// Adds a section title to each popular post section
-function enp_default_pop_posts_section_title($html, $pop_posts){
-    $html = '<h3>Most '.$pop_posts->btn_past_tense_name.' Posts</h3>'.$html;
+// Adds a section title and the UL to each popular post section
+function enp_default_pop_posts_loop_before($html, $pop_posts){
+    $html = '<h3 class="enp-popular-posts-section-title enp-popular-posts-section-title--'.$pop_posts->btn_slug.'">Most '.$pop_posts->btn_past_tense_name.' Posts</h3>
+            <ul class="enp-popular-posts-list enp-popular-posts-list--'.$pop_posts->btn_slug.'">'.$html;
     return $html;
 }
-add_filter('enp_popular_posts_loop_before_html', 'enp_default_pop_posts_section_title', 10, 2);
+add_filter('enp_popular_posts_loop_before_html', 'enp_default_pop_posts_loop_before', 10, 2);
+
+// Adds closing UL to each popular post section
+function enp_default_pop_posts_loop_after($html, $pop_posts){
+    $html .= '</ul>';
+    return $html;
+}
+add_filter('enp_popular_posts_loop_after_html', 'enp_default_pop_posts_loop_after', 10, 2);
 
 
 // creates our popular post list items
 function enp_default_pop_post_html($html, $pop_id, $pop_count, $pop_posts){
-    $html .= '<li class="enp-popular-list-item enp-popular-list-item--'.$pop_posts->btn_slug.'">
-                <a class="enp-popular-list-link enp-popular-list-link--'.$pop_posts->btn_slug.'" href="'.get_permalink($pop_id).'">
-                    <span class="enp-popular-list-title enp-popular-list-title--'.$pop_posts->btn_slug.'">'.get_the_title($pop_id).'</span>
-                     <span class="enp-popular-list-btn-count enp-popular-list-btn-count--'.$pop_posts->btn_slug.'">'.$pop_count.'</span>
+    $html .= '<li class="enp-popular-posts-list-item enp-popular-posts-list-item--'.$pop_posts->btn_slug.'">
+                <a class="enp-popular-posts-list-link enp-popular-posts-list-link--'.$pop_posts->btn_slug.'" href="'.get_permalink($pop_id).'">
+                    <span class="enp-popular-posts-list-title enp-popular-posts-list-title--'.$pop_posts->btn_slug.'">'.get_the_title($pop_id).'</span>
+                     <span class="enp-popular-posts-list-btn-count enp-popular-posts-list-btn-count--'.$pop_posts->btn_slug.'">'.$pop_count.'</span>
                 </a>
             </li>';
     return $html;
@@ -58,6 +68,5 @@ function pop_comments_wrap($html, $this){
 }
 add_filter('enp_popular_comments_loop_wrap', 'pop_comments_wrap', 10, 2);
 add_filter('enp_popular_posts_loop_wrap', 'pop_comments_wrap', 10, 2);*/
-
 
 ?>
