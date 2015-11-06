6df0dd0f5a14c779c6132f36f4213dfcf4c480fb
Jerry <jones.jeremydavid@gmail.com>
2015-11-06 08:54:05 -0600

    added variable for how many posts to return on popular_loop

--- front-end/popular_buttons/Enp_Popular_Loop.php
+++ front-end/popular_buttons/Enp_Popular_Loop.php
@@ -1,7 +1,7 @@
 <?
 class Enp_Popular_Loop extends Enp_Popular_Buttons {
 
-    public function popular_loop() {
+    public function popular_loop($posts_per_page = 5) {
         if($this->have_popular()) {
 
             $enp_popular_html = '';
@@ -10,16 +10,15 @@ class Enp_Popular_Loop extends Enp_Popular_Buttons {
             do_action( 'enp_popular_loop_before', $this );
 
             // run comments or posts loop
-            $enp_popular_html = $this->{'popular_'.$this->label.'_loop'}();
+            $enp_popular_html = $this->{'popular_'.$this->label.'_loop'}($posts_per_page);
 
             do_action( 'enp_popular_loop_after', $this );
 
             return $enp_popular_html;
         }
-
     }
 
-    public function popular_comments_loop() {
+    public function popular_comments_loop($posts_per_page = 5) {
         $enp_popular_html = '';
 
         do_action( 'enp_popular_comments_loop_before', $this );
@@ -46,13 +45,13 @@ class Enp_Popular_Loop extends Enp_Popular_Buttons {
         return apply_filters('enp_popular_comments_loop_wrap', $enp_popular_html, $this);
     }
 
-    public function popular_posts_loop() {
+    public function popular_posts_loop($posts_per_page = 5) {
         $enp_popular_html = '';
 
         do_action( 'enp_popular_posts_loop_before', $this );
         $enp_popular_html = apply_filters( 'enp_popular_posts_loop_before_html', $enp_popular_html, $this );
 
-
+        $i = 0;
         foreach($this->{'popular_'.$this->label} as $pop) {
             $enp_popular_post_html = '';
             $pop_id = $pop[$this->singular_label.'_id'];
@@ -64,6 +63,11 @@ class Enp_Popular_Loop extends Enp_Popular_Buttons {
 
             do_action( 'enp_popular_post_after' ,$pop_id, $pop_count );
 
+            // we only want 5 as a default, with the option to increase $posts_per_page later
+            $i++;
+            if($posts_per_page <= $i) {
+                break;
+            }
         }
 
         do_action( 'enp_popular_posts_loop_after', $this );
