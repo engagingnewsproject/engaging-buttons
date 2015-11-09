b0f15b0bc4073e63eacb87525f21813128377a44
Jerry <jones.jeremydavid@gmail.com>
2015-11-09 15:48:21 -0600

    Turned API into class

--- inc/data-api.php
+++ inc/data-api.php
@@ -1,33 +1,43 @@
 <?
 
-class Enp_Data_API {
+class Enp_Send_Data_API {
     public $site_url;
 
     public function __construct() {
         $this->site_url = site_url();
+        $data = new Enp_Button(array('btn_slug'=>'respect'));
+        $data_string = json_encode($data);
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'http://dev/enp-api/api.php');
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            'Content-Type: application/json',
+            'Content-Length: ' . strlen($data_string))
+        );
+
+        $result = curl_exec($ch);
+
+        curl_close($ch);
+
+        // testing
+        var_dump(json_decode($result));
+
+        /*
+        if we want to process the response
+        $json_result = json_decode($result);
+        var_dump($json_result->data->site_url);*/
+
     }
 
 }
 
-//var_dump(new Enp_Data_API());
+new Enp_Send_Data_API();
 
-$data = array("site_url" => site_url());
-$data_string = json_encode($data);
 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, 'http://dev/enp-api/api.php');
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-    'Content-Type: application/json',
-    'Content-Length: ' . strlen($data_string))
-);
 
-$result = curl_exec($ch);
 
-curl_close($ch);
-
-var_dump($result);
 
 ?>
