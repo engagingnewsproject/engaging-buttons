433e4fff1090cb281f75293d5ff68f94483a1629
Jerry <jones.jeremydavid@gmail.com>
2016-01-29 16:05:43 -0600

    updated version

--- inc/Enp_Button_Loader.php
+++ inc/Enp_Button_Loader.php
@@ -79,7 +79,7 @@ class Enp_Button_Loader {
     *
     */
     public function enp_btn_register_scripts() {
-        $version = '1.0.1';
+        $version = '1.0.2';
 
         // get our style choice from the database
         $enp_btn_style = get_option('enp_button_style');
